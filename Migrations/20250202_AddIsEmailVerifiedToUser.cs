df0a7ee6f69973ad3f71cdbb33c919aab12aa613
DarkFlame7777 <player859@yandex.by>
2026-02-03 02:12:26 +0300

    Add files via upload

---
+++ Migrations/20250202_AddIsEmailVerifiedToUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Task4.Migrations
+{
+    public partial class AddIsEmailVerifiedToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailVerified",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEmailVerified",
+                table: "Users");
+        }
+    }
+}
